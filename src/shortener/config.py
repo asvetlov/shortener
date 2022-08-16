d9e2c005d75cbef766da7b622a836412fa0b9ac1
Andrew Svetlov <andrew.svetlov@gmail.com>
2022-08-16 13:06:58 +0200

    Add prometheus setup

--- src/shortener/config.py
+++ src/shortener/config.py
@@ -2,5 +2,8 @@ from pydantic import BaseSettings, Field, RedisDsn
 
 
 class Config(BaseSettings):
+    http_host: str = Field(default="localhost", env="HTTP_HOST")
     http_port: int = Field(default=8080, env="HTTP_PORT")
     redis_url: RedisDsn = Field(default="redis://redis", env="REDIS_URL")
+    prometheus_host: str = Field(default="localhost", env="PROMETHEUS_HOST")
+    prometheus_port: int = Field(default=8081, env="PROMETHEUS_PORT")
--- src/shortener/web.py
+++ src/shortener/web.py
@@ -3,13 +3,16 @@ import json
 from collections.abc import MappingView
 from dataclasses import asdict, is_dataclass
 from functools import partial
-from typing import Any, AsyncIterator
+from typing import Any, AsyncIterator, Awaitable, Callable
 
 import jinja2
 import structlog
 from aiohttp import web
 from aiohttp_jinja2 import render_template
 from aiohttp_jinja2 import setup as setup_jinja
+from prometheus_async.aio import time
+from prometheus_async.aio.web import start_http_server
+from prometheus_client import Histogram
 from yarl import URL
 
 from .config import Config
@@ -21,6 +24,10 @@ DB: contextvars.ContextVar[Database] = contextvars.ContextVar("DB")
 
 
 ROUTES = web.RouteTableDef()
+
+REQ_TIME = Histogram(
+    "req_time_seconds", "time spent in requests", ["method", "endpoint"]
+)
 
 
 def json_default(obj: Any) -> Any:
@@ -34,7 +41,11 @@ def json_default(obj: Any) -> Any:
         raise TypeError(type(obj))
 
 
+REQ_TIME_INDEX = REQ_TIME.labels("get", "/")
+
+
 @ROUTES.get("/")
+@time(REQ_TIME_INDEX)
 async def index(request: web.Request) -> web.Response:
     added_short = request.query.get("added_short")
     if added_short:
@@ -53,18 +64,36 @@ async def index(request: web.Request) -> web.Response:
         return render_template("index.jinja2", request, dct)
 
 
+REQ_TIME_CREATE = REQ_TIME.labels("post", "/")
+
+
 @ROUTES.post("/")
+@time(REQ_TIME_INDEX)
 async def create(request: web.Request) -> web.Response:
     form = await request.post()
-    url = form["url"]
-    assert isinstance(url, str)
-    short = await DB.get().register(URL(url))
-    new_url = URL.build(path="/", query={"added_short": short, "added_url": url})
-    logger.info("web.created", short=short, url=url)
+    url_str = form.get("url")
+    if not url_str:
+        logger.error("web.create.fail.absent", url=url_str)
+        raise web.HTTPBadRequest(text="URL is absent")
+    if not isinstance(url_str, str):
+        logger.error("web.create.fail.type", url=url_str, url_type=type(url_str))
+        raise web.HTTPBadRequest(text="URL is malfolmed")
+    try:
+        url = URL(url_str)
+    except Exception as exc:
+        logger.error("web.create.fail.malformed", url=url)
+        raise web.HTTPBadRequest(text="URL is malfolmed")
+    short = await DB.get().register(url)
+    new_url = URL.build(path="/", query={"added_short": short, "added_url": url_str})
+    logger.info("web.creat.ok", short=short, url=url_str)
     raise web.HTTPSeeOther(location=new_url)
 
 
+REQ_TIME_REDIRECT = REQ_TIME.labels("get", "/{short}")
+
+
 @ROUTES.get("/{short}")
+@time(REQ_TIME_INDEX)
 async def redirect(request: web.Request) -> web.Response:
     short = request.match_info["short"]
     url = await DB.get().redirect(short)
@@ -75,8 +104,23 @@ async def redirect(request: web.Request) -> web.Response:
     raise web.HTTPSeeOther(location=url)
 
 
+@web.middleware
+async def errors_handler(
+    request: web.Request,
+    handler: Callable[[web.Request], Awaitable[web.StreamResponse]],
+) -> web.StreamResponse:
+    try:
+        return await handler(request)
+    except web.HTTPException as exc:
+        logger.exception("web.error.http")
+        raise
+    except Exception:
+        logger.critical("web.error.unhandler")
+        raise web.HTTPInternalServerError()
+
+
 async def init(config: Config) -> web.Application:
-    app = web.Application()
+    app = web.Application(middlewares=[errors_handler])
     setup_jinja(app, loader=jinja2.PackageLoader(__package__, "templates"))
     app.add_routes(ROUTES)
 
@@ -87,11 +131,20 @@ async def init(config: Config) -> web.Application:
         yield
         await db.close()
 
+    async def setup_prometheus(app: web.Application) -> AsyncIterator[None]:
+        srv = await start_http_server(
+            addr=config.prometheus_host, port=config.prometheus_port
+        )
+        app["prometheus"] = srv
+        yield
+        await srv.close()
+
     app.cleanup_ctx.append(setup_db)
+    app.cleanup_ctx.append(setup_prometheus)
     return app
 
 
 def main() -> None:
     config = Config()
-    logger.info("web.serve", port=config.http_port)
-    web.run_app(init(config), port=config.http_port)
+    logger.info("web.serve", host=config.http_host, port=config.http_port)
+    web.run_app(init(config), host=config.http_host, port=config.http_port)
